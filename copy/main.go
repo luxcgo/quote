7e06ea949901cafa58fb573ababa721f85b08191
luxcgo <luxcgo@gmail.com>
2022-08-21 23:05:59 +0800

    cp

--- copy/main.go
+++ copy/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
 func (d *Driver) SetTrips(trips []*Trip) {
 	d.trips = make([]*Trip, len(trips))
@@ -18,7 +21,7 @@ type Driver struct {
 	trips []*Trip
 }
 
-func main() {
+func main1() {
 	d1 := &Driver{}
 	trips := []*Trip{{"A"}}
 	d1.SetTrips(trips)
@@ -26,4 +29,21 @@ func main() {
 	trips[0].Name = "...."
 
 	fmt.Println(d1.trips[0])
+}
+
+func main() {
+	t1 := &Trip{
+		Name: "t1",
+	}
+
+	t2 := new(Trip)
+
+	*t2 = *t1
+	log.Println(t1.Name)
+	log.Println(t2.Name)
+
+	t2.Name = "t2"
+
+	log.Println(t1.Name)
+	log.Println(t2.Name)
 }
